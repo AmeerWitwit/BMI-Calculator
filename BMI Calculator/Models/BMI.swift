85b964851619b6d2a1ef38e34f9359f144cd5039
Ameer Witwit <ameer.witwit98@gmail.com>
2022-06-28 16:57:01 +0300

    Initial Commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Ameer Witwit on 21/12/2021.
+
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
